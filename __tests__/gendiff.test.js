b3c78741d80ff7cbc5f105fa9309223c9290fe8a
metastasio <myaldzina.m@gmail.com>
2023-05-23 17:44:12 +0300

    Added

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,20 @@
+import url from 'url';
+import fs from 'fs';
+import path from 'path';
+import genDiff from '../index.js';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) =>
+  path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) =>
+  fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+const testJsonOutput = readFile('testJsonOutput.txt');
+const file1 = './__fixtures__/file1.json';
+const file2 = './__fixtures__/file2.json';
+
+test('genDiff', () => {
+  expect(genDiff(file1, file2)).toEqual(testJsonOutput);
+});
