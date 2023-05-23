237ca322d262429f9acc961413e884ea484934e9
metastasio <myaldzina.m@gmail.com>
2023-05-23 18:02:54 +0300

    Modified gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,10 +6,8 @@ import genDiff from '../index.js';
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const getFixturePath = (filename) =>
-  path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) =>
-  fs.readFileSync(getFixturePath(filename), 'utf-8');
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const testJsonOutput = readFile('testJsonOutput.txt');
 const file1 = './__fixtures__/file1.json';
