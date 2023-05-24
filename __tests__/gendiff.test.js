8353071c5c897e93310aae665adcea6f1dbff1a2
metastasio <myaldzina.m@gmail.com>
2023-05-24 18:33:40 +0300

    Added new tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,7 +1,7 @@
 import url from 'url';
 import fs from 'fs';
 import path from 'path';
-import genDiff from '../index.js';
+import genDiff from '../src/index.js';
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,7 +12,10 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8'
 const testJsonOutput = readFile('testJsonOutput.txt');
 const file1 = './__fixtures__/file1.json';
 const file2 = './__fixtures__/file2.json';
+const file1yml = './__fixtures__/file1yml.yml';
+const file2yml = './__fixtures__/file2yml.yml';
 
 test('genDiff', () => {
   expect(genDiff(file1, file2)).toEqual(testJsonOutput);
+  expect(genDiff(file1yml, file2yml)).toEqual(testJsonOutput);
 });
