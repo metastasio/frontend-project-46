66ada0d9ac6cb281643ac34027e1f652472abadf
metastasio <myaldzina.m@gmail.com>
2023-06-08 22:00:54 +0300

    Modified stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,52 +1,62 @@
 import _ from 'lodash';
 
-const stringify = (value, replacer = '  ', spacesCount = 1) => {
-  const iter = (currentValue, depth) => {
-    if (!_.isObject(currentValue)) {
-      return `${currentValue}`;
-    }
-    const indentSize = depth * spacesCount;
-    const currentIndent = replacer.repeat(indentSize);
-    const bracketIndent = replacer.repeat(indentSize - spacesCount);
-    const lines = Object.entries(currentValue).map(
-      ([key, val]) => `${currentIndent}${key}: ${iter(val, depth + 1)}`,
-    );
-    return ['{', ...lines, `${bracketIndent}}`].join('\n');
-  };
-  return iter(value, 1);
+const replacer = ' ';
+const spaces = 4;
+const offset = 2;
+const indent = (depth) => replacer.repeat(depth * spaces - offset);
+const bracketIndent = (depth) => replacer.repeat(depth * spaces - spaces);
+
+const stringify = (value, depth) => {
+  if (!_.isObject(value)) {
+    return `${value}`;
+  }
+  const lines = Object.entries(value).map(
+    ([key, val]) => `${indent(depth)}  ${key}: ${stringify(val, depth + 1)}`,
+  );
+  return ['{', ...lines, `${bracketIndent(depth)}}`].join('\n');
 };
 
 const stylish = (diff) => {
-  const result = diff.reduce((acc, item) => {
-    if (item.type === 'deleted') {
-      const newKey = `- ${item.key}`;
-      acc[newKey] = item.value;
-      return acc;
-    }
-    if (item.type === 'added') {
-      const newKey = `+ ${item.key}`;
-      acc[newKey] = item.value;
-      return acc;
-    }
-    if (item.type === 'unchanged') {
-      const newKey = `  ${item.key}`;
-      acc[newKey] = item.value;
-      return acc;
-    }
-    if (item.type === 'node') {
-      const newKey = `  ${item.key}`;
-      acc[newKey] = stylish(item.children);
-      return acc;
-    }
-    if (item.type === 'changed') {
-      const newKey1 = `- ${item.key}`;
-      acc[newKey1] = item.value1;
-      const newKey2 = `+ ${item.key}`;
-      acc[newKey2] = item.value2;
-      return acc;
-    }
-  }, {});
-  return stringify(result);
+  const iter = (currentValue, depth) => {
+    const result = currentValue.map((item) => {
+      if (item.type === 'deleted') {
+        return `${indent(depth)}- ${item.key}: ${stringify(
+          item.value,
+          depth + 1,
+        )}`;
+      }
+      if (item.type === 'added') {
+        return `${indent(depth)}+ ${item.key}: ${stringify(
+          item.value,
+          depth + 1,
+        )}`;
+      }
+      if (item.type === 'unchanged') {
+        return `${indent(depth)}  ${item.key}: ${stringify(
+          item.value,
+          depth + 1,
+        )}`;
+      }
+      if (item.type === 'node') {
+        return `${indent(depth)}  ${item.key}: ${iter(
+          item.children,
+          depth + 1,
+        )}`;
+      }
+      if (item.type === 'changed') {
+        const first = `${indent(depth)}- ${item.key}: ${stringify(
+          item.value1,
+          depth + 1,
+        )}`;
+        const second = `${indent(depth)}+ ${item.key}: ${stringify(
+          item.value2,
+          depth + 1,
+        )}`;
+        return `${first}\n${second}`;
+      }
+    });
+    return ['{', ...result, `${bracketIndent(depth)}}`].join('\n');
+  };
+  return iter(diff, 1);
 };
-
 export default stylish;
