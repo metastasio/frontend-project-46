c9014b3cd8dcdc022f5bd55e4bf2622b9f4446ed
metastasio <myaldzina.m@gmail.com>
2023-06-12 14:13:20 +0300

    Modified functions

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,10 +6,8 @@ import genDiff from '../src/index.js';
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const getFixturePath = (filename) =>
-  path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) =>
-  fs.readFileSync(getFixturePath(filename), 'utf-8');
+const getPath = (file) => path.join(__dirname, '..', '__fixtures__', file);
+const readFile = (file) => fs.readFileSync(getPath(file), 'utf-8');
 
 const testNestedOutput = readFile('testJsonOutputnested.txt');
 const testPlain = readFile('testJsonOutputnested_plain.txt');
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -18,41 +18,38 @@ const stringify = (value, depth) => {
 
 const stylish = (diff) => {
   const iter = (currentValue, depth) => {
-    const result = currentValue.map((item) => {
-      if (item.type === 'deleted') {
-        return `${indent(depth)}- ${item.key}: ${stringify(
-          item.value,
-          depth + 1,
-        )}`;
-      }
-      if (item.type === 'added') {
-        return `${indent(depth)}+ ${item.key}: ${stringify(
-          item.value,
-          depth + 1,
-        )}`;
-      }
-      if (item.type === 'unchanged') {
-        return `${indent(depth)}  ${item.key}: ${stringify(
-          item.value,
-          depth + 1,
-        )}`;
-      }
-      if (item.type === 'node') {
-        return `${indent(depth)}  ${item.key}: ${iter(
-          item.children,
-          depth + 1,
-        )}`;
-      }
-      if (item.type === 'changed') {
-        const first = `${indent(depth)}- ${item.key}: ${stringify(
-          item.value1,
-          depth + 1,
-        )}`;
-        const second = `${indent(depth)}+ ${item.key}: ${stringify(
-          item.value2,
-          depth + 1,
-        )}`;
-        return `${first}\n${second}`;
+    const result = currentValue.flatMap((item) => {
+      switch (item.type) {
+        case 'deleted':
+          return `${indent(depth)}- ${item.key}: ${stringify(
+            item.value,
+            depth + 1,
+          )}`;
+        case 'added':
+          return `${indent(depth)}+ ${item.key}: ${stringify(
+            item.value,
+            depth + 1,
+          )}`;
+        case 'unchanged':
+          return `${indent(depth)}  ${item.key}: ${stringify(
+            item.value,
+            depth + 1,
+          )}`;
+        case 'node':
+          return `${indent(depth)}  ${item.key}: ${iter(
+            item.children,
+            depth + 1,
+          )}`;
+        case 'changed':
+          return `${indent(depth)}- ${item.key}: ${stringify(
+            item.value1,
+            depth + 1,
+          )}\n${indent(depth)}+ ${item.key}: ${stringify(
+            item.value2,
+            depth + 1,
+          )}`;
+        default:
+          return [];
       }
     });
     return ['{', ...result, `${bracketIndent(depth)}}`].join('\n');
